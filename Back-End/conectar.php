dd7665aa12aebf3da793a9113b7cf4073f4eb9e3
Gustavo Alexandre da Silva <gustavoale1759@gmail.com>
2025-09-28 12:19:36 -0300

    Update conectar.php

--- Back-End/conectar.php
+++ Back-End/conectar.php
@@ -1,44 +1,45 @@
 <?php
-// conectar.php
-
 class Conectar extends PDO 
 {
     private static $instancia; 
-private $query;
-private $host = "127.0.0.1";
-private $usuario = "root";
-private $senha = "";
-private $db = "produtosvixel";
+    private $query;
+    private $host = "127.0.0.1";
+    private $usuario = "root";
+    private $senha = "";
+    private $db = "produtosvixel";
 
-public function __construct()
-{
-    parent::__construct("mysql:host=$this->host;dbname=$this->db","$this->usuario", "$this->senha");
-}
-
-public static function getInstancia()
-{
-    if (!isset(self::$instacia))
+    public function __construct()
     {
         try {
-            self::$instacia = new Conectar();
-            echo 'Conectado com sucesso';
-
-        } catch (Exception $e) {
-       echo 'Erro ao conectar '   ;
-       exit();  
+            parent::__construct("mysql:host=$this->host;dbname=$this->db", $this->usuario, $this->senha);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Erro na conexão: ' . $e->getMessage();
+            exit();
         }
     }
-    return self::$instacia;
+    public static function getInstancia()
+    {
+        if (!isset(self::$instancia)) {
+            try {
+                self::$instancia = new Conectar();
+            } catch (Exception $e) {
+                echo 'Erro ao conectar: ' . $e->getMessage();
+                exit();  
+            }
+        }
+        return self::$instancia;
+    }
+    public function sql($query, $params = [])
+    {
+        try {
+            $stmt = self::getInstancia()->prepare($query);
+            $stmt->execute($params);
+            return $stmt;
+        } catch (PDOException $e) {
+            echo 'Erro na consulta: ' . $e->getMessage();
+            return false;
+        }
+    }
 }
-public function sql ($query)
-{
-    $this->getInstancia();
-    $this-> query = $query;
-    $stmt = $pdo->prepare($this->query);
-$stmt -> execute();
-$pdo = null;
-
-}
-}
-
 ?>
